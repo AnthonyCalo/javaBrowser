01a56194dd79823f8fe70401c1aea5cdba4446ec
Anthony Calo <acalo4554@gmail.com>
2022-11-27 18:56:18 -0500

    First commit

---
+++ src/application/Browser.java
@@ -0,0 +1,18 @@
+package application;
+import javafx.application.Application;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class Browser extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		TabPane tabs = new TabPane();
+		tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_Tabs);
+		
+		
+	}
+	
+
+}
---
+++ src/application/SinglePage.java
@@ -0,0 +1,9 @@
+package application;
+
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class SinglePage {
+	private WebView webview;
+	private WebEngine webEngine;
+}
